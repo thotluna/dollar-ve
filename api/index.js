b9cf2d4304eea226d717ea8e728204eeca3beb48
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-12 19:26:55 -0400

    Add route root to api

--- api/index.js
+++ api/index.js
@@ -6,19 +6,45 @@ import { getLastBcv, getLastsTwitter } from './services'
 const app = new Hono()
 
 app.get('/', (context) => {
+  return context.json([
+    {
+      endpoint: '/current',
+      description: 'Current prices of the dollar with respect to the bolivar'
+    },
+    {
+      endpoint: '/bcv',
+      description: 'historical prices of the dollar according to BCV'
+    },
+    {
+      endpoint: '/twitter',
+      description: 'Historical prices of the dollar according to several users on Twitter'
+    }
+  ])
+})
+
+app.get('/current', (context) => {
   const result = {
     bcv: getLastBcv(),
     twitter: getLastsTwitter()
   }
-  return context.json(result)
+  return context.json({
+    return: '/',
+    data: result
+  })
 })
 
 app.get('/twitter', (context) => {
-  return context.json(twitter)
+  return context.json({
+    return: '/',
+    data: twitter
+  })
 })
 
 app.get('/bcv', (context) => {
-  return context.json(bcv)
+  return context.json({
+    return: '/',
+    data: bcv
+  })
 })
 
 export default app
