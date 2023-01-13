572e91d490c9ba8138cf51d3c4b482edfc5abcb9
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-12 20:23:59 -0400

    Fix: does not upgrade db. forever create new

--- db/index.js
+++ db/index.js
@@ -1,25 +1,15 @@
-import { writeFile, readFile, access } from 'node:fs/promises'
+import { writeFile, readFile } from 'node:fs/promises'
 import path from 'node:path'
+
+import { logInfo } from '../utils/log.js'
 
 const DB_PATH = path.join(process.cwd(), './db/')
 
 export async function readDBFile (dbName) {
-  await existsFile(dbName)
   return readFile(`${DB_PATH}/${dbName}.json`, 'utf-8').then(JSON.parse)
 }
 
 export async function writeDBFile (dbName, data) {
+  logInfo(`Save in db ${dbName} ${data}`)
   return await writeFile(`${DB_PATH}/${dbName}.json`, JSON.stringify(data, null, 2), 'utf-8')
-}
-
-async function cratedFile (dbName) {
-  await writeDBFile(dbName, [])
-}
-
-async function existsFile (dbName) {
-  try {
-    await access(`${DB_PATH}/${dbName}.json`, true)
-  } catch (error) {
-    await cratedFile(dbName)
-  }
 }
