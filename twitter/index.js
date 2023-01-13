226711e022994f40c1b4780f8b3927329720948b
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-13 13:15:55 -0400

    add new env var

--- twitter/index.js
+++ twitter/index.js
@@ -43,7 +43,8 @@ const getData = (dataDirtty) => {
 export async function getDataByFetch (url) {
   const token = process.env.token
   const PRUEBA = process.env.PRUEBA
-  logSuccess({ token, PRUEBA })
+  logSuccess(token)
+  logSuccess(`prueba: ${PRUEBA}`)
   const options = {
     headers: {
       authorization: `Bearer ${token}`
