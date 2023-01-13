cee99a033c3dfde650a332d66db41f6511a4e77b
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-13 02:51:12 -0400

    Remove dotenv

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import dotenv from 'dotenv'
 import { scrapeBCV } from './scrapebcv/scrapebcv.js'
 import { scraperTwitter } from './twitter/index.js'
 
-dotenv.config()
 scrapeBCV()
 scraperTwitter()
