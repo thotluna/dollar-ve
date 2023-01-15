d27ebd4ca370f2b09aa67fc48ead0918e2494413
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-14 20:27:39 -0400

    Add config const Api url

--- src/services/currencies.js
+++ src/services/currencies.js
@@ -1,10 +1,11 @@
+import { Config } from '../config'
+
 export async function getCurrencies() {
 	try {
-		const response = await fetch('http://localhost:8787/current')
+		const response = await fetch(`${Config.API_URL_PRODUCTION}${Config.API_ENDPOINT_CURRENT}`)
 		const json = await response.json()
 		return json.data
 	} catch (e) {
-		// enviar el error a un servicio de reporte de errores
 		return null
 	}
 }
