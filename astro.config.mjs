6a2905f5e6157e3561119ea1e44055fa85865612
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-17 16:54:41 -0400

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
 
+import sitemap from '@astrojs/sitemap'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	site: 'https://dollar-ve.pages.dev/',
+  integrations: [
+		tailwind(),
+		sitemap()
+	]
+
 })
