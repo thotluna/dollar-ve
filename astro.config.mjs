f870f3f0d54d152c1f22caf91997cdcec8228b48
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-17 15:14:01 -0400

    Move charts to public/images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
-import.meta.env.NODE_VERSION = '18'
 
 // https://astro.build/config
 export default defineConfig({
