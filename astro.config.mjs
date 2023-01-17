1fd7c6e1e35a17538a172b9c24517b5d68dfec51
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-17 14:13:09 -0400

    add var env node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
+import.meta.env.NODE_VERSION = '18'
 
 // https://astro.build/config
 export default defineConfig({
