29fcbab025f538bc4aa1a8419e64f1e77887a39b
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-19 19:08:38 -0400

    Add char to currency

--- src/components/ChartCurrency.jsx
+++ src/components/ChartCurrency.jsx
@@ -1,59 +1,14 @@
 
-import { useEffect, useRef } from 'react'
-import Chart from 'chart.js/auto'
+import { useRef } from 'react'
+import { useChartBar } from '../hooks/useChartBar'
 
 export function ChartCurrency({ list }) {
 	const canvasRef = useRef()
 
 	const key = list.map(value => value.date).reverse()
 	const dollars = list.map(value => value.dollar).reverse()
-	console.log({ key, dollars })
-	const min = Math.min(...dollars.map(value => Number(value))) - 0.5
-	const max = Math.max(...dollars.map(value => Number(value))) + 0.5
 
-	useEffect(() => {
-		const canvas = canvasRef.current
-		const ctx = canvas.getContext('2d')
-
-		Chart.defaults.font.size = 10
-		Chart.defaults.borderColor = '#94A3B8'
-		Chart.defaults.color = '#CBD5E1'
-
-		const chart = new Chart(ctx, {
-			type: 'bar',
-			data: {
-				labels: key,
-				datasets: [{
-					label: null,
-					data: dollars
-				}]
-			},
-			options: {
-				responsive: true,
-				plugins: {
-					legend: {
-						display: false
-					}
-				},
-				scales: {
-					x: {
-						display: true,
-						title: {
-							display: false
-						}
-					},
-					y: {
-						display: true,
-						title: {
-							display: false
-						},
-						min,
-						max
-					}
-				}
-			}
-		})
-	})
+	useChartBar(canvasRef, key, dollars)
 
 	return (
 		<div style={{ width: '200px' }}>
