a535f246c7c80a364bc998778544cad778a0231a
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-19 18:46:38 -0400

    Add react and chartjs libs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
-
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-export default defineConfig({
-	site: 'https://dollar-ve.pages.dev/',
-  integrations: [
-		tailwind(),
-		sitemap()
-	]
+import react from '@astrojs/react'
 
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://dollar-ve.pages.dev/',
+  integrations: [tailwind(), sitemap(), react()]
 })
