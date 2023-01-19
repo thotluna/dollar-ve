ab2da2828b1ffb27ce5e77ba71076d0dce15ba9a
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-01-19 19:08:24 -0400

    Add new custom hook for chars bar

---
+++ src/hooks/useChartBar.js
@@ -0,0 +1,55 @@
+import Chart from 'chart.js/auto'
+import { useEffect } from 'react'
+
+export function useChartBar(ref, keys, values) {
+	const min = Math.min(...values.map(value => Number(value))) - 0.5
+	const max = Math.max(...values.map(value => Number(value))) + 0.5
+
+	const chart = async () => {
+		const canvas = ref.current
+		const ctx = canvas.getContext('2d')
+
+		Chart.defaults.font.size = 10
+		Chart.defaults.borderColor = '#94A3B8'
+		Chart.defaults.color = '#94A3B8'
+
+		new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: keys,
+				datasets: [{
+					label: null,
+					data: values
+				}]
+			},
+			options: {
+				responsive: true,
+				plugins: {
+					legend: {
+						display: false
+					}
+				},
+				scales: {
+					x: {
+						display: true,
+						title: {
+							display: false
+						}
+					},
+					y: {
+						display: true,
+						title: {
+							display: false
+						},
+						min,
+						max
+					}
+				}
+			}
+		})
+	}
+
+	useEffect(() => {
+		chart()
+	})
+}
